21bee3f09a65e2d0bbd0c644c3d26bedfe4c65b3
Zhuang Wang <zhuangw@amazon.com>
2026-08-14 14:55:00 -0700

    Guard release-aligned Quick Start

---
+++ tests/unit/test_readme_quickstart.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+
+ROOT = Path(__file__).parents[2]
+README = ROOT / "README.md"
+
+
+def test_release_quickstart_clones_installed_version_tag():
+    readme = README.read_text()
+
+    assert 'version("lm-resiliency")' in readme
+    assert '--branch "v${LM_RESILIENCY_VERSION}"' in readme
+    assert "git clone --depth 1 https://github.com/LMResiliency/lm-resiliency.git" not in readme
