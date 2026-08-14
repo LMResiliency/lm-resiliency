4f91b5f647cbc1a43e4180df3aae1bcd6dfcc243
Zhuang Wang <zhuangw@amazon.com>
2026-08-14 15:01:09 -0700

    Remove stale typing suppression

--- lm_resiliency/recovery.py
+++ lm_resiliency/recovery.py
@@ -45,7 +45,7 @@ def build_recovery_decision(
         if allow_collective:
             checkpoint_step = int(checkpoint_manager.find_latest(mode))  # type: ignore[attr-defined]
         elif hasattr(checkpoint_manager, "local_recovery_step"):
-            checkpoint_step = int(  # type: ignore[attr-defined]
+            checkpoint_step = int(
                 checkpoint_manager.local_recovery_step(mode)
             )
         elif mode is RecoveryMode.RECOVERY_VERIFIED:
